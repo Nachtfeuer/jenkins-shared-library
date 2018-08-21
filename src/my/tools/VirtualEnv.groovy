932b1aac0cb2a60e09e2a4601da7ba71f36792c7
Thomas Lehmann <thomas.lehmann.private@gmail.com>
2018-08-21 05:51:17 +0200

    Python virtual environment DSL and tool class

---
+++ src/my/tools/VirtualEnv.groovy
@@ -0,0 +1,91 @@
+package my.tools
+
+/**
+ * Python virtual environment wrapper
+ */
+class VirtualEnv extends Base {
+    /** folder name of the virtual environment (default: venv). */
+    private String theFolderName = 'venv'
+    /** list of requirements to install with pip */
+    private final List<String> requirements = []
+
+    /**
+     * Initializing gradle with Jenkinsfile script instance only.
+     */
+    VirtualEnv(final script) {
+        super(script)
+    }
+
+    /**
+     * @return currently configured folder name for Python virtual environment
+     */
+    String getFolderName() {
+        this.theFolderName
+    }
+
+    /**
+     * @return list of configured requirements.
+     */
+    List<String> getRequirements() {
+        this.requirements.asImmutable()
+    }
+
+    /**
+     * Configuration of the Python virtual environment.
+     * @param config either a Map or named parameter <b>folderName</b> which defines folder for
+     *        virtual environment is supported.
+     */
+    VirtualEnv configure(final Map config) {
+        if (config.containsKey('folderName') && config.folderName instanceof String
+                                             && !config.folderName.isEmpty()) {
+            this.theFolderName = config.folderName
+        } else if (config.containsKey('requirements') && config.requirements instanceof List
+                                                      && config.requirements.every { it instanceof String }) {
+            this.requirements.clear()
+            this.requirements.addAll(config.requirements)
+        }
+        this
+    }
+
+    /**
+     * Provides block to define scope for a virtualenv.
+     * @param body closure to create a block.
+     * @return whatever the inner block does return.
+     */
+    def process(final Closure body) {
+        this.script.withEnv(this.environment()) {
+            this.setup()
+            def result = body()
+            this.teardown()
+            result
+        }
+    }
+
+    /**
+     * Create a virtualenv.
+     */
+    private void setup() {
+        this.script.sh(script:'virtualenv ' + this.theFolderName)
+        this.requirements.each {
+            this.script.sh(script:"python -m pip install ${it}")
+        }
+    }
+
+    /**
+     * Remove the virtualenv.
+     */
+    private void teardown() {
+        this.script.sh(script:'rm -rf ' + this.theFolderName)
+    }
+
+    /**
+     * @return Adjusted environment variables required for virtual environment.
+     */
+    private List<String> environment() {
+        final VIRTUAL_ENV_PATH = this.script.pwd() + '/' + this.theFolderName
+        [
+            "PATH=${VIRTUAL_ENV_PATH}/bin:${script.env.PATH}",
+            "VIRTUAL_ENV=${VIRTUAL_ENV_PATH}",
+        ]
+    }
+}
--- test/src/my/tools/MockScript.groovy
+++ test/src/my/tools/MockScript.groovy
@@ -6,14 +6,39 @@ package my.tools
 class MockScript {
     /** list of calls */
     private final List calls = []
+    /** currently defined environment variables */
+    private final env = [PATH:'']
+    /** current working path. */
+    private final String currentPath = System.getProperty('user.dir')
 
-    /** access to list off call. **/
+    /** readonly access to list off calls. **/
     def getCalls() {
-        this.calls
+        this.calls.asImmutable()
     }
 
     /** mock of the Jenkins sh DSL function. */
     def sh(final Map config) {
         this.calls.add(['sh', config])
+    }
+
+    /** mock of the Jenkins pwd DSL function. */
+    def pwd() {
+        this.calls.add(['pwd', this.currentPath])
+        this.currentPath
+    }
+
+    /** mock of the withEnv DSL function. */
+    def withEnv(final List<String> environment, final Closure body) {
+        this.calls.add(['withEnv', environment])
+        def oldEnv = [:]
+        oldEnv.putAll(this.env)
+        environment.each {
+            def tokens = it.split('=')
+            this.env.put(tokens[0], tokens[1])
+        }
+        def result = body()
+        this.env.clear()
+        this.env.putAll(oldEnv)
+        result
     }
 }
