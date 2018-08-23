fad9c0b2ebe8a01c09b7050a822574b95c9ff7f4
Thomas Lehmann <thomas.lehmann.private@gmail.com>
2018-08-23 05:40:16 +0200

    xgradel.publish DSL added

--- vars/xgradle.groovy
+++ vars/xgradle.groovy
@@ -8,3 +8,12 @@ void build() {
     def api = new Gradle(this)
     api.clean().check()
 }
+
+/**
+ * Running junit and jacoco DSL.
+ */
+void publish() {
+    // creating instance of Gradle Groovy wrapper for Gradle command line tool
+    def api = new Gradle(this)
+    api.publish()
+}
