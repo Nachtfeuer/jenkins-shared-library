922a77200b558ab6df6be2c27d32b1cf10b91ae9
Thomas Lehmann <thomas.lehmann.private@gmail.com>
2018-09-16 18:39:52 +0200

    #15 local code execution (part one)

--- src/my/dsl/Jenkins.groovy
+++ src/my/dsl/Jenkins.groovy
@@ -3,7 +3,11 @@ package my.dsl
 /**
  * DSL Implementation for local use.
  */
+@SuppressWarnings(['AbstractClassWithoutAbstractMethod', 'Println', 'JavaIoPackageAccess'])
 abstract class Jenkins extends Script {
+    /** newline constant */
+    private final static String NEWLINE = '\n'
+
     /**
      * Print a message to the stdout.
      *
@@ -13,20 +17,43 @@ abstract class Jenkins extends Script {
         println(message)
     }
 
+    /**
+     * Executing a shell command.
+     *
+     * @param config a map with followng fields: <b>script</b> a string representing the command.
+     *        <b>returnStdout</b> an optional boolean indicating that the output is returned;
+     *        the default is "false". <b>returnStatus</b> an optional boolean field indicating
+     *        that the exit code for the shell command is returned.
+     * @return depends on parameters: null if neither exit code nor output is wanted otherwise
+     *         an int or a string.
+     */
     def sh(final Map config) {
         if (config.returnStdout ?: false) {
             def result = new ShellExecutor().execute(config.script)
-            result.lines.join('\n')
+            result.lines.join(NEWLINE)
         } else {
             def result = new ShellExecutor().execute(config.script)
-            println(result.lines.join('\n'))
+            println(result.lines.join(NEWLINE))
+            null
         }
     }
 
+    /**
+     * Writing text to a file.
+     *
+     * @param config a map with following fields: <b>file</b> path and filename of file,
+     *        <b>text</b> the text to write into that file.
+     */
     void writeFile(final Map config) {
         new File(config.file).text = config.text
     }
 
+    /**
+     * Reading text from a file.
+     *
+     * @param config a map with following fields: <b>file</b> path and filename of file.
+     * @return file content as string.
+     */
     String readFile(final Map config) {
         new File(config.file).text
     }
--- src/my/dsl/ShellExecutor.groovy
+++ src/my/dsl/ShellExecutor.groovy
@@ -9,7 +9,7 @@ class ShellExecutor {
      * @return map with two fields: exitCode (int) and lines (list of strings)
      */
     Map execute(final String command) {
-        def result = [exitCode: 0, lines:[]]
+        def result = [exitCode:0, lines:[]]
         def process = new ProcessBuilder(['bash', '-c', command])
             .redirectErrorStream(true)
             .start()
@@ -18,6 +18,6 @@ class ShellExecutor {
         process.inputStream.eachLine { result.lines.add(it) }
         process.waitFor()
         result.exitCode = process.exitValue()
-        return result
+        result
     }
 }
